19f2dec9d2d50d1106390ef51cd5d6daf8447e98
Hoàng Đức Đạt <noir@pop-os.localdomain>
2020-09-24 11:09:12 +0700

    resolvers and migration create

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constances";
 import mikroConfig from "./mikro-orm.config";
@@ -5,6 +6,11 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
@@ -12,11 +18,31 @@ const main = async () => {
 
   const app = express();
 
+  let RedisStore = connectRedis(session);
+  let redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({ client: redisClient, disableTouch: true }),
+      secret: "sdflshfdslfghsadlgfhsdlgs",
+      resave: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        httpOnly: true,
+        sameSite: "lax",
+        secure: __prod__, // cookie only works in https
+      },
+      saveUninitialized: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
+    context: ({ req, res }) => ({ em: orm.em, req, res }),
   });
   apolloServer.applyMiddleware({ app });
 
